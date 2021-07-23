e9da0fe978be5c69156b13e9aae00b02ee90d151
alexeyvokin <alvo14@mail.ru>
2021-07-23 14:10:24 +0700

    Initialize stub

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+$green: #42b983;
+
+#app {
+  text-align: center;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: $--font-weight-bold;
+    &.router-link-exact-active {
+      color: $green;
+    }
+  }
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  lintOnSave: true,
+  devServer: {
+    open: true,
+    watchOptions: {
+      poll: true,
+    },
+    // Configure proxy, more: https://github.com/chimurai/http-proxy-middleware#http-proxy-options
+    // proxy: 'http://localhost:1337/'
+  },
+
+  css: {
+    loaderOptions: {
+      scss: {
+        prependData: '@import "~@/assets/scss/vars";',
+      },
+    },
+  },
+};
